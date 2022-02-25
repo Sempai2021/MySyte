cb16a310dd6a934c8c6536658feb5fc4dc6fc21f
Тёмыч <88252870+Sempai2021@users.noreply.github.com>
2022-02-25 19:38:05 +0300

    Delete calmar directory

--- calmar/files/wows.js
+++
@@ -1,8 +0,0 @@
-new WOW().init(
-	{
-                      boxClass:     'wow',      // default
-                      animateClass: 'animate__animated', // default
-                      offset:       1500,          // default
-                      mobile:       true,       // default
-                      live:         true        // default
-                    });
