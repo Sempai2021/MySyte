ab6a0f94265e0383828198a27ef980ba94206832
Artoym <tem4ik.capper@gmail.com>
2022-02-25 19:32:34 +0300

    add

---
+++ calmar/files/wows.js
@@ -0,0 +1,8 @@
+new WOW().init(
+	{
+                      boxClass:     'wow',      // default
+                      animateClass: 'animate__animated', // default
+                      offset:       1500,          // default
+                      mobile:       true,       // default
+                      live:         true        // default
+                    });
